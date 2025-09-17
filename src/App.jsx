6b5d4fd19142f4e533824fd5929ce63bc5d2e611
Anirudh2244 <anirudhdubey2244@gmail.com>
2025-09-17 14:44:29 +0530

    more logo added

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,21 @@ import CircularText from "./CircularText";
 
 
 const imageLogos = [
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/eureka.png?updatedAt=1757933147240", alt: "Company 2", href: "" },
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/foxtale.png?updatedAt=1757933147149", alt: "Company 3", href: "" },
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/genetic-nutritions.png?updatedAt=1757933147120", alt: "Company 2", href: "" },
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/tac.png?updatedAt=1757933147038", alt: "Company 3", href: "" },
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/good-bug.png?updatedAt=1757933147044", alt: "iLife", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/solara.png?updatedAt=1758100052488", alt: "solara", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/concious-chemist.png?updatedAt=1758099561327", alt: "concious-chemist", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/wow.png?updatedAt=1758100347247", alt: "wow", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/ilife.png?updatedAt=1757933146720", alt: "ilife", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/eureka.png?updatedAt=1757933147240", alt: "eureka", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/mantra(1).png?updatedAt=1758100203683", alt: "mantra", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/tac.png?updatedAt=1757933147038", alt: "tac", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/foxtale.png?updatedAt=1757933147149", alt: "foxtale", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/genetic-nutritions.png?updatedAt=1757933147120", alt: "genetic-nuttition", href: "" },
   { src: "https://ik.imagekit.io/adsrc2244/Zorshour/my-muse.png?updatedAt=1757933146957", alt: "My Muse", href: "" },
-  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/ilife.png?updatedAt=1757933146720", alt: "Genetic Nutritions", href: "" },
-
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/carbamide.png?updatedAt=1758099561569", alt: "carbamide", href: "" },
+  { src: "https://ik.imagekit.io/adsrc2244/Zorshour/good-bug.png?updatedAt=1757933147044", alt: "good-bug", href: "" }
 ];
+
+
 
 function App() {
   const [showHero, setShowHero] = useState(true);
